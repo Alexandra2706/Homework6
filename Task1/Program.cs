e4c161869fd54790e9957a86042e54f9dd183bd9
Александра Гаврилова <cx_76@mail.ru>
2023-10-07 10:22:43 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int EnterNumber(string msg)
+{
+    Console.Write(msg);
+    if (!int.TryParse(Console.ReadLine(), out var number))
+    {
+        Console.Write("Ошибка ввода!");
+        Environment.Exit(0);
+    }
+    return number;
+}
+
+int CountPositiveNumbers(int[] array_numbers)
+{
+    int sum_positive = 0;
+    for (int i = 0; i < array_numbers.Length; i++)
+    {
+        if (array_numbers[i] > 0) sum_positive++;
+    }
+    return sum_positive;
+}
+
+
+int amount_numbers = EnterNumber("Введите количество чисел: ");
+if (amount_numbers <= 0)
+{
+    Console.Write("Количество чисел должно быть больше 0!");
+    Environment.Exit(0);
+}
+int[] numbers_arr = new int[amount_numbers];
+for (int i = 0; i < amount_numbers; i++)
+{
+    numbers_arr[i] = EnterNumber($"Введите {i + 1} число: ");
+}
+
+Console.Write($"Количество положительных чисел = {CountPositiveNumbers(numbers_arr)}");
---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int EnterParam(string msg)
+{
+    Console.Write(msg);
+    if (!int.TryParse(Console.ReadLine(), out var number))
+    {
+        Console.Write("Ошибка ввода!");
+        Environment.Exit(0);
+    }
+    return number;
+}
+
+void IntersectionPoint(int k1, int b1, int k2, int b2)
+{
+    if (k1 != k2)
+    {
+        double x = (double)(b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        double y2 = k2 * x + b2;
+        Console.Write($"Точка пересечения прямых: x = {x}, y = {y}");
+    }
+    else Console.WriteLine("Эти прямые не пересекаются!");
+}
+
+Console.WriteLine("Задайте параметры уравнения для первой прямой:");
+int k1 = EnterParam($"Введите коэффициент k1 для первой прямой: ");
+int b1 = EnterParam($"Введите коэффициент b1 для первой прямой: ");
+Console.WriteLine("");
+Console.WriteLine("Задайте параметры уравнения для второй прямой:");
+int k2 = EnterParam($"Введите коэффициент k2 для второй прямой: ");
+int b2 = EnterParam($"Введите коэффициент b2 для второй прямой: ");
+
+IntersectionPoint(k1, b1, k2, b2);
